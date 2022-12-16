99e221b1c399736ffe2c3739ec204b1cabf3a50c
pansoic <117926661+pansoic@users.noreply.github.com>
2022-12-15 18:25:00 -0800

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'7b6841d8-7d8d-4fd7-9a49-030d2fceda46'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
